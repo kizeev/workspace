fc73f00c68270ea9265e97643593dd34626f0575
kizei <kizalvic@gmail.com>
2024-09-17 10:43:43 +0300

    improve nvim tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,23 @@
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+        float = {
+            enable = true,
+            open_win_config = {
+                relative = "editor",
+                width = 30,
+                height = 40,
+                row = 0,
+            }
+        }
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = false,
+    },
 })
