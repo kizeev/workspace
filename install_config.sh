f998e8ebb4d16b7ac5f7c2ed3c56f2e256981e42
kizeev <kizalvic@gmail.com>
2025-02-11 15:08:55 +0300

    edit script, add fonts, p10k

--- install_config.sh
+++ install_config.sh
@@ -2,8 +2,8 @@
 
 # install work tools
 sudo pacman -S zsh bitwarden neovim tmux obsidian gcc xclip tree ctags fd \
-    ripgrep postgresql alacritty flameshot docker nodejs npm dbeaver \
-    base-devel git wireguard-tools --noconfirm --needed
+   ripgrep postgresql alacritty flameshot docker nodejs npm dbeaver \
+   base-devel git wireguard-tools --noconfirm --needed
 
 
 # install AUR
@@ -20,13 +20,19 @@ yay -S google-chrome postman-bin slack-desktop
 chsh -s /bin/zsh
 
 
+# install fonts
+mkdir -p "~/.local/share/fonts"
+find "workspace/fonts" \( -name "*.ttf" -o -name "*.otf" -o -name "*.ttc" \) -type f -print0 | \
+    xargs -0 -I {} cp "{}" "~/.local/share/fonts/"
+
 # alacritty config
 mv ~/.config/alacritty/alacritty.toml \
     ~/.config/alacritty/alacritty_default.toml
-cp -r alacritty ~/.config
+cp -r ~/workspace/alacritty ~/.config
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme \
     ~/.config/alacritty/themes
+cp ~/workspace/.p10k.zsh ~/
 
 
 # git config
@@ -35,18 +41,17 @@ echo "https://kizeev:$git_token@github.com" >> ~/.git-credentials
 
 cat << EOF > ~/.gitconfig
 [user]
-    name = kizeev
-    email = kizalvic@gmail.com
+   name = kizeev
+   email = kizalvic@gmail.com
 [credential]
-    helper = store
+   helper = store
 EOF
 
 
 # nvim config
-cp -r nvim ~/.config
+cp -r ~/workspace/nvim ~/.config
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim -c "PackerSync"
 
 
 # tmux config
